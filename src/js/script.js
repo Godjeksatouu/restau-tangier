dad0eb15cf59c064e55873c99946e49172d10d9b
Godjeksatouu <godjeksatou@gmail.com>
2024-12-18 13:49:38 +0100

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/data/restaurants.json')
+    fetch('/restau-tangier/data/restaurants.json')
         .then(response => response.json())
         .then(data => {
             const container = document.getElementById('restaurant-container');
